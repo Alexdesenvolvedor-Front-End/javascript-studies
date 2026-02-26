07d55726460db71caab91432cd24820c9adab4d5
Alexdesenvolvedor-Front-End <133884002+Alexdesenvolvedor-Front-End@users.noreply.github.com>
2026-02-26 17:54:45 -0300

    Curso de JS em andamento.

--- arquivoAulaLogicaJS/script.js
+++ arquivoAulaLogicaJS/script.js
@@ -1,3 +1,25 @@
 
 
-alert("ola mundo!")
+let nome = prompt("Qual o seu nome?");
+console.log(`Bem vindo! ${nome}`);
+
+
+
+/*
+
+Tipos de dados
+
+Alguns tipos de dados que uma variável pode conter;
+STRINGS
+NÚMERO INTEIRO
+NÚMERO REAL
+BOLEANO
+ARRAY
+OBJETOS
+
+*/
+
+
+
+
+
---
+++ conversão entre tipos/js/script.js
@@ -0,0 +1,33 @@
+
+
+
+// Operadores aritméticos básicos
+
+
+/*
+
+| Operador | Nome                      | Exemplo  | Resultado |
+| -------- | ------------------------- | -------- | --------- |
+| `+`      | Adição                    | `5 + 3`  | `8`       |
+| `-`      | Subtração                 | `5 - 3`  | `2`       |
+| `*`      | Multiplicação             | `5 * 3`  | `15`      |
+| `/`      | Divisão                   | `10 / 2` | `5`       |
+| `%`      | Resto da divisão (módulo) | `10 % 3` | `1`       |
+| `**`     | Potenciação               | `2 ** 3` | `8`       |
+
+*/
+
+
+
+
+// Pedir para o usuário digitar um número
+let n1 = prompt("Digite um número: ");
+n1 = parseFloat(n1);
+
+// Pedir para o usuário digitar outro número
+let n2 = prompt("Digite outro número: ");
+n2 = parseFloat(n2);
+
+// Mostrar a soma
+console.log(`Total ${n1 + n2}`);
+
