1ffd5443098db0bf55167402b4f228fefa1340bd
Alexdesenvolvedor-Front-End <133884002+Alexdesenvolvedor-Front-End@users.noreply.github.com>
2026-02-27 18:57:26 -0300

    Curso JS em execução

--- operadores artiméticos/script.js
+++ operadores artiméticos/script.js
@@ -13,18 +13,16 @@
 
 // Pedir para o usuário digitar uma nota
 
-// let n1 = prompt("Digite um nota: ");
-// n1 = parseFloat(n1);
+let n1 = prompt("Digite um nota: ");
+n1 = parseFloat(n1);
 
 // // Pedir para o usuário digitar outra nota
-// let n2 = prompt("Digite outra nota: ");
-// n2 = parseFloat(n2);
+let n2 = prompt("Digite outra nota: ");
+n2 = parseFloat(n2);
 
-// media = (n1 + n2) / 2;
+media = (n1 + n2) / 2;
 
-// console.log(`A sua média é: ${media}`);
-
-
+console.log(`A sua média é: ${media}`);
 
 
 
@@ -33,25 +31,8 @@
 
 
 
-// Pedir para o usuário digitar um número: 
-
-let n1 = prompt("Didite um número: ");
 
 
-// Converta em um número inteiro
-n1 = parseInt(n1);
-
-// Pedir para o usuário digitar outro número: 
-
-let n2 = prompt("Digite outro número: ");
-
-// Converta em um número inteiro
-n2 = parseInt(n2);
-
-
-// Mostrar na tela o resto da divisão de (n1) por (n2) é: (resultado);
-resultado = n1 % n2;
-console.log(`O resultado do resto é: ${resultado} `);
 
 
 
