f6beadb1ddeeede4417acbe53a3d924feaeb279a
Alexdesenvolvedor-Front-End <133884002+Alexdesenvolvedor-Front-End@users.noreply.github.com>
2026-02-27 18:36:23 -0300

    Curso de JS em execução

---
+++ operadores artiméticos/script.js
@@ -0,0 +1,59 @@
+
+
+// Operadores Aritméticos
+
+// + ADIÇÃO
+// - SUBTRAÇÃO
+// * MULTIPLICAÇÃO
+// / DIVISÃO
+// % MÓDULO
+// ** EXPONENCIAÇÃO
+// ++ INCREMENTO
+// -- DECREMENTO
+
+// Pedir para o usuário digitar uma nota
+
+// let n1 = prompt("Digite um nota: ");
+// n1 = parseFloat(n1);
+
+// // Pedir para o usuário digitar outra nota
+// let n2 = prompt("Digite outra nota: ");
+// n2 = parseFloat(n2);
+
+// media = (n1 + n2) / 2;
+
+// console.log(`A sua média é: ${media}`);
+
+
+
+
+
+
+
+
+
+
+// Pedir para o usuário digitar um número: 
+
+let n1 = prompt("Didite um número: ");
+
+
+// Converta em um número inteiro
+n1 = parseInt(n1);
+
+// Pedir para o usuário digitar outro número: 
+
+let n2 = prompt("Digite outro número: ");
+
+// Converta em um número inteiro
+n2 = parseInt(n2);
+
+
+// Mostrar na tela o resto da divisão de (n1) por (n2) é: (resultado);
+resultado = n1 % n2;
+console.log(`O resultado do resto é: ${resultado} `);
+
+
+
+
+
