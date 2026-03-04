2f6bc952cf696ff0bb4c604920b5bf711480e747
Alexdesenvolvedor-Front-End <133884002+Alexdesenvolvedor-Front-End@users.noreply.github.com>
2026-03-04 17:31:18 -0300

    Curso JS em execução

---
+++ controle de decisão switch/js/script.js
@@ -0,0 +1,23 @@
+
+
+
+
+let n = prompt("Digite um número: ");
+
+n = parseInt(n);
+
+
+switch (n) {
+    case 1:
+        console.log("Boa escolha");
+        break;
+    case 2:
+        console.log("Legal");
+        break;
+    case 3:
+        console.log("Caramba");
+    default:
+        console.log("Escolha de 1 a 3");
+
+
+}
