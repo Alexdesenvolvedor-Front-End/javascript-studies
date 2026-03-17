04200be8af505ce62f0f2561d6d8e3e08ac102bb
Alexdesenvolvedor-Front-End <133884002+Alexdesenvolvedor-Front-End@users.noreply.github.com>
2026-03-17 15:43:11 -0300

    curso de jacascript  em execução

---
+++ exercicio extrutura de repetição for  ordem decrescente mostrando somente os números pares/js/script.js
@@ -0,0 +1,11 @@
+
+
+
+// Conte de 50 até 1 (decrescente) usando o for
+
+
+for (let i = 60; i > 1; i--) {
+    if (i % 2 === 0) {
+        console.log(`Mostre somente os números pares: ${i}`);
+    }
+}
---
+++ exercicio extrutura de repetição for conte de 1 a 10/js/script.js
@@ -0,0 +1,9 @@
+
+
+
+// Conte de 1 a 50 usando a extrutura de repetição for
+
+
+for (let i = 1; i < 50; i++) {
+    console.log(`Repeti pra mim...${i}`);
+}
