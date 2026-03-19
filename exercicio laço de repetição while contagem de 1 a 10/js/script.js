52830ce4e57a9fcf506356087eb454c839cc5c7b
Alexdesenvolvedor-Front-End <133884002+Alexdesenvolvedor-Front-End@users.noreply.github.com>
2026-03-19 18:05:10 -0300

    Curso javascript em construção

---
+++ exercicio laço de repetição while contagem de 1 a 10/js/script.js
@@ -0,0 +1,12 @@
+
+
+// Exercicio laço de repetição while contagem de 1 a 10
+
+
+
+let i = 1;
+
+while (i <= 10) {
+    console.log(`Número: ${i}`);
+    i++;
+}
---
+++ exercicio laço de repetição while numeros pare de 0 a 20/js/script.js
@@ -0,0 +1,14 @@
+
+
+
+// Exercicio laço de repetição while número de 1 a 20
+
+
+let i = 1;
+
+while (i <= 20) {
+    if (i % 2 === 0) {
+        console.log(`Numeros pares: ${i}`);
+    }
+    i++;
+}
