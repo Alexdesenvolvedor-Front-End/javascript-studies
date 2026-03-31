cc3ab489639704f8fe9317946d2d70eca9001399
Alexdesenvolvedor-Front-End <133884002+Alexdesenvolvedor-Front-End@users.noreply.github.com>
2026-03-31 09:28:41 -0300

    curso javascript em construção

---
+++ exercicio laço de repetição while desafio completo nível profissonal iniciante/js/script.js
@@ -0,0 +1,19 @@
+
+
+
+// Desafio Completo (nível profissional iniciante)
+
+// Somar os números digitados
+// Contar quantos números foram digitados
+// Calcular a média
+// Validar se o usuário digitou o número válido
+// Parar quando o usuário digitar o número 0
+
+let soma = 0;
+let contador = 0;
+
+
+
+let n1 = prompt("Digite um número: ");
+n1 = parseInt(n1);
+
