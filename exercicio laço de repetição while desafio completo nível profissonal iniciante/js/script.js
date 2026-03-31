4453eff471d893cedc738c4abaa0c6a1954de1e7
Alexdesenvolvedor-Front-End <133884002+Alexdesenvolvedor-Front-End@users.noreply.github.com>
2026-03-31 18:46:19 -0300

    Curso javascript em construção

--- exercicio laço de repetição while desafio completo nível profissonal iniciante/js/script.js
+++ exercicio laço de repetição while desafio completo nível profissonal iniciante/js/script.js
@@ -11,9 +11,27 @@
 
 let soma = 0;
 let contador = 0;
+let media = 0;
 
 
 
 let n1 = prompt("Digite um número: ");
 n1 = parseInt(n1);
 
+
+while (n1 !== 0){
+    if(isNaN(n1)){
+        console.log(`Nâo é um número valido!`)
+    }else{
+        soma+= n1;
+        contador++;
+    }
+
+    n1 = prompt("Digite outro número: ");
+    n1 = parseInt(n1);
+
+}
+
+if(contador > 0){
+    media = soma / contador;
+}
--- exercicio laço de repetição while jogo numero secreto/js/script.js
+++
@@ -1,9 +0,0 @@
-
-
-// Exercicio número secreto com dica (maior ou menor)
-
-// exercicio secreto ex: 15
-
-// Ex: Usuário tenta adivinhar enquanto errar o programa diz "Maior " se o número for maior  "Menor se o número for menor"
-//  Qaundo acertar mostra mensagem de parabéns , moatra a quatidade de tentativas
-
