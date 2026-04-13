b60e6df2458da09951d7620f8a9afc01b47328da
Alexdesenvolvedor-Front-End <133884002+Alexdesenvolvedor-Front-End@users.noreply.github.com>
2026-04-13 15:35:48 -0300

    Curso js em construção

--- exercicio laço de repetição while desafio completo nível profissonal iniciante/js/script.js
+++ exercicio laço de repetição while desafio completo nível profissonal iniciante/js/script.js
@@ -18,20 +18,8 @@ let media = 0;
 let n1 = prompt("Digite um número: ");
 n1 = parseInt(n1);
 
-
-while (n1 !== 0){
-    if(isNaN(n1)){
-        console.log(`Nâo é um número valido!`)
-    }else{
-        soma+= n1;
-        contador++;
+while(n1 !== 0){
+    if(isNaN (n1)){
+        console.log("Valor inválido!");
     }
-
-    n1 = prompt("Digite outro número: ");
-    n1 = parseInt(n1);
-
-}
-
-if(contador > 0){
-    media = soma / contador;
 }
